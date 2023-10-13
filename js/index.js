425c53d796a96e06924e34d28894bc550e1c8b61
Daniel Lopez <neo_19_8@hotmail.com>
2023-10-12 19:06:18 -0600

    updateUi

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@ import { setPokemon } from "./pokedex.js"
 const $form = document.querySelector('#form') 
 const $next = document.querySelector('#next-pokemon') 
 const $prev = document.querySelector('#prev-pokemon') 
-const $pokedex = document.querySelector('#pokedex') 
+const $pokedex = document.querySelector('#pokedex')
+const $random = document.querySelector('#random-pokemon')
 
 
  
 $form.addEventListener('submit', handleSubmit)
 $next.addEventListener('click', handleNextPokemon)
 $prev.addEventListener('click', handlePrevPokemon)
+$random.addEventListener('click', handleRandomPokemon)
 
 
 let activePokemon =null
@@ -32,3 +34,11 @@ async function handlePrevPokemon() {
        const id = (activePokemon === null || activePokemon.id === 1) ? 898 : activePokemon.id - 1
       activePokemon = await setPokemon(id)
 }
+
+async function handleRandomPokemon() {
+  const minId = 1;
+  const maxId = 898;
+  const randomId = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+  activePokemon = await setPokemon(randomId);
+}
+
