f720131a7c07dcb8c13117e1bc155edfb427d073
Daniel Lopez <neo_19_8@hotmail.com>
2023-10-12 18:02:30 -0600

    updateUi

---
+++ js/pokedex.js
@@ -0,0 +1,40 @@
+import { getPokemon, getSpecies } from "./api.js"
+
+const $image = document.querySelector("#image")
+function setImage(image) {
+    $image.src = image
+}
+
+
+const $description = document.querySelector("#description")
+function setDescription(text) {
+    $description.textContent = text
+}
+
+
+const $screen = document.querySelector("#screen");
+function loader(isLoading = false) {
+    const img = isLoading ? 'url(./images/loading.gif)': ''
+    $screen.style.backgroundImage = img
+}
+
+
+export async function findPokemon(id) {
+  const pokemon = await getPokemon(id)
+  const species = await getSpecies(id)
+  const description = species.flavor_text_entries.find((flavor) => flavor.language.name === "es")
+  return {
+    sprites: pokemon.sprites.front_default,
+    description: description.flavor_text,
+  }
+}
+
+export async function setPokemon(id) {
+    //prender loader
+    loader(true)
+    const pokemon = await findPokemon(id)
+    //apagar loader
+    loader(false)
+    setImage(pokemon.sprites)
+    setDescription(pokemon.description)
+}
