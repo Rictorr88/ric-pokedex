223e72b2e218a883c6ff205648370244d1b5b42f
Daniel Lopez <neo_19_8@hotmail.com>
2023-10-13 21:43:06 -0600

    final

--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
-import { setPokemon } from "./pokedex.js"
-const $form = document.querySelector('#form') 
-const $next = document.querySelector('#next-pokemon') 
-const $prev = document.querySelector('#prev-pokemon') 
-const $pokedex = document.querySelector('#pokedex')
-const $random = document.querySelector('#random-pokemon')
+import "./charts.js"
+import { setPokemon, setImage } from "./pokedex.js";
+const $form = document.querySelector("#form");
+const $next = document.querySelector("#next-pokemon");
+const $prev = document.querySelector("#prev-pokemon");
+const $nextImage = document.querySelector("#next-image");
+const $prevImage = document.querySelector("#prev-image");
+const $pokedex = document.querySelector("#pokedex");
+const $random = document.querySelector("#random-pokemon");
 
+$form.addEventListener("submit", handleSubmit);
+$next.addEventListener("click", handleNextPokemon);
+$prev.addEventListener("click", handlePrevPokemon);
+$nextImage.addEventListener("click", handleNextImage);
+$prevImage.addEventListener("click", handlePrevImage);
+$random.addEventListener("click", handleRandomPokemon);
 
- 
-$form.addEventListener('submit', handleSubmit)
-$next.addEventListener('click', handleNextPokemon)
-$prev.addEventListener('click', handlePrevPokemon)
-$random.addEventListener('click', handleRandomPokemon)
-
-
-let activePokemon =null
+let activePokemon = null;
 
 async function handleSubmit(event) {
-      $pokedex.classList.add('is-open')
-      event.preventDefault()
-      const form = new FormData($form)
-      const id = form.get("id")
-      activePokemon = await setPokemon(id)
+  $pokedex.classList.add("is-open");
+  event.preventDefault();
+  const form = new FormData($form);
+  const id = form.get("id");
+  activePokemon = await setPokemon(id);
 }
-
 
 async function handleNextPokemon() {
-      const id = (activePokemon === null || activePokemon.id === 898) ? 1 : activePokemon.id + 1
-      activePokemon = await setPokemon(id)
+  const id = activePokemon === null || activePokemon.id === 898 ? 1 : activePokemon.id + 1;
+  activePokemon = await setPokemon(id);
 }
 
-
 async function handlePrevPokemon() {
-       const id = (activePokemon === null || activePokemon.id === 1) ? 898 : activePokemon.id - 1
-      activePokemon = await setPokemon(id)
+  const id = activePokemon === null || activePokemon.id === 1 ? 898 : activePokemon.id - 1;
+  activePokemon = await setPokemon(id);
 }
 
 async function handleRandomPokemon() {
@@ -42,3 +42,23 @@ async function handleRandomPokemon() {
   activePokemon = await setPokemon(randomId);
 }
 
+let activeSprite = 0;
+function handleNextImage() {
+  if (activePokemon === null) return false;
+  if (activeSprite >= activePokemon.sprites.length - 1) {
+    activeSprite = 0;
+    return setImage(activePokemon.sprites[activeSprite]);
+  }
+  activeSprite = activeSprite + 1;
+  return setImage(activePokemon.sprites[activeSprite]);
+}
+
+function handlePrevImage() {
+  if (activePokemon === null) return false;
+  if (activeSprite <= 0) {
+    activeSprite = activePokemon.sprites.length - 1;
+    return setImage(activePokemon.sprites[activeSprite]);
+  }
+  activeSprite = activeSprite - 1;
+  return setImage(activePokemon.sprites[activeSprite]);
+}
--- js/pokedex.js
+++ js/pokedex.js
@@ -1,7 +1,8 @@
 import { getPokemon, getSpecies } from "./api.js"
+import { createChart } from "./charts.js"
 
 const $image = document.querySelector("#image")
-function setImage(image) {
+export function setImage(image) {
     $image.src = image
 }
 
@@ -18,25 +19,55 @@ function loader(isLoading = false) {
     $screen.style.backgroundImage = img
 }
 
+const $light= document.querySelector("#light")
+function speech(text) {
+  const utterance = new SpeechSynthesisUtterance(text)
+  utterance.lang = 'es'
+  speechSynthesis.speak(utterance)
+  $light.classList.add('is-animated')
+
+  utterance.addEventListener('end', () => {
+    $light.classList.remove('is-animated')
+  }) 
+}
+
 
 export async function findPokemon(id) {
   const pokemon = await getPokemon(id)
   const species = await getSpecies(id)
   const description = species.flavor_text_entries.find((flavor) => flavor.language.name === "es")
+  const sprites = [pokemon.sprites.front_default]
+
+  const stats = pokemon.stats.map(item => item.base_stat)
+  for (const item in pokemon.sprites) {
+    if (item !== 'front_default' && item !== 'other' && item !== 'versions' && pokemon.sprites[item]) {
+      sprites.push(pokemon.sprites[item])
+    }
+  }
+
   return {
-    sprites: pokemon.sprites.front_default,
+    sprites,
     description: description.flavor_text,
     id: pokemon.id,
+    name: pokemon.name,
+    stats,
   }
 }
 
+let activeChart = null
 export async function setPokemon(id) {
     //prender loader
     loader(true)
     const pokemon = await findPokemon(id)
     //apagar loader
     loader(false)
-    setImage(pokemon.sprites)
+
+    setImage(pokemon.sprites[0])
     setDescription(pokemon.description)
+    speech(`${pokemon.name}. ${pokemon.description}`) 
+    if (activeChart instanceof Chart) {
+      activeChart.destroy()
+    }
+    activeChart = createChart(pokemon.stats)
     return pokemon
 }
